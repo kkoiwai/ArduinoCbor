5a8af7794c869be5a82c4fdfbba5719810c5e9be
Kosuke Koiwai <kk@kocko.org>
2020-01-04 01:13:44 +0900

    Enabled Integer as a key

--- src/CborObject.cpp
+++ src/CborObject.cpp
@@ -14,6 +14,10 @@ CborVariant CborObject::get(const char* key) {
   return CborVariant(buffer, cn_cbor_mapget_string(raw, key));
 }
 
+CborVariant CborObject::get(const int8_t key) {
+  return CborVariant(buffer, cn_cbor_mapget_int(raw, key));
+}
+
 void CborObject::set(const char* key, CborVariant value) {
   cn_cbor_errback err;
 
@@ -33,6 +37,28 @@ void CborObject::set(const char* key, CborObject value) {
 }
 
 void CborObject::set(const char* key, CborArray value) {
+  set(key, CborVariant(buffer, value));
+}
+
+void CborObject::set(const int8_t key, CborVariant value) {
+  cn_cbor_errback err;
+
+  cn_cbor_mapput_int(raw, key, value.raw, &buffer.context, &err);
+}
+
+void CborObject::set(const int8_t key, const char* value) {
+  set(key, CborVariant(buffer, value));
+}
+
+void CborObject::set(const int8_t key, CBOR_INT_T value) {
+  set(key, CborVariant(buffer, value));
+}
+
+void CborObject::set(const int8_t key, CborObject value) {
+  set(key, CborVariant(buffer, value));
+}
+
+void CborObject::set(const int8_t key, CborArray value) {
   set(key, CborVariant(buffer, value));
 }
 
--- src/CborObject.h
+++ src/CborObject.h
@@ -10,12 +10,19 @@ class CborObject {
   CborObject(CborBuffer& buffer, cn_cbor* raw=0);
 
   CborVariant get(const char* key);
+  CborVariant get(const int8_t key);
 
   void set(const char* key, CborVariant value);
   void set(const char* key, const char* value);
   void set(const char* key, CBOR_INT_T value);
   void set(const char* key, CborObject value);
   void set(const char* key, CborArray value);
+
+  void set(const int8_t key, CborVariant value);
+  void set(const int8_t key, const char* value);
+  void set(const int8_t key, CBOR_INT_T value);
+  void set(const int8_t key, CborObject value);
+  void set(const int8_t key, CborArray value);
 
   size_t encode(uint8_t* data, size_t size);
 
